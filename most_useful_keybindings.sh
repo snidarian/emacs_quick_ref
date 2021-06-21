0b0977bde341b36c5d246f82227f01929e7b489a
snidarian <cephalopod31956@gmail.com>
2021-06-21 12:57:49 -0400

    help bindings and quit binding

--- most_useful_keybindings.sh
+++ most_useful_keybindings.sh
@@ -1,16 +1,28 @@
 # most useful emacs keybindings
 # C stands for the control key and M stands for the Meta or Alt key.
 
+# The command description is listed in all caps and the command name comes after the hyphen
 
 
-# STARTS EMACS (This keybinding I have set up through zsh and is not part of emacs)
+# IDENTIFY KEY COMMAND KEYBINDING
+# C-h ? C [COMMAND KEY SEQUENCE]
+
+# STARTS EMACS - (This keybinding I have set up through zsh and is not part of emacs)
 # C-x C-e
 
-# CLOSE EMACS
+# CLOSE EMACS - save-buffers-kill-terminal
 # C-x C-c
 
-# SAVE CURRENT DOCUMENT/FILE
+# SAVE CURRENT DOCUMENT/FILE - save-buffer
 # C-x C-s
+
+# CANCEL CURRENT ACTION/COMMAND - 
+# C-g
+
+# LIST OTHER BUFFERS - list-buffers
+# C-x C-b
+
+
 
 
 
