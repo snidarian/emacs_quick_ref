43df1c5b977ad5c38e1501c5ec8cbdb4aaeb79cc
snidarian <cephalopod31956@gmail.com>
2021-06-21 12:37:46 -0400

    most basic

---
+++ most_useful_keybindings.sh
@@ -0,0 +1,32 @@
+# most useful emacs keybindings
+# C stands for the control key and M stands for the Meta or Alt key.
+
+
+
+# STARTS EMACS (This keybinding I have set up through zsh and is not part of emacs)
+# C-x C-e
+
+# CLOSE EMACS
+# C-x C-c
+
+# SAVE CURRENT DOCUMENT/FILE
+# C-x C-s
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
